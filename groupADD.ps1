89317fbd7b7e005eb37d0b9c726710c915300a16
kowwwalski <flazh@yandex.ru>
2016-07-21 14:49:33 +0300

    Add files via upload

---
+++ groupADD.ps1
@@ -0,0 +1,8 @@
+﻿#Create csv-file with user credentials(FIO) & get samaccount
+import-csv -path %credenlist%.csv
+ForEach-Object{get-adobject -filter "name -like '*$($_.name)*'" -properties *}
+Select-Object samaccountname|export-csv %output%.csv
+#convert to txt without first row (name) & put it into the var
+$var = Get-Content %somedir%\yourlist.txt
+#add users from txt to ADgroup
+Add-ADGroupMember "group" -Member $var
