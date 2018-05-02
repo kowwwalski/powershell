8f197b5a0b247202fa3b59420cb575f41240ac86
Maxim Chinenkof <flazh@yandex.ru>
2018-05-02 21:51:47 +0300

    Delete groupADD.ps1

--- groupADD.ps1
+++
@@ -1,8 +0,0 @@
-﻿#Create csv-file with user credentials(FIO) & get samaccount (insert "name" in 1st row)
-import-csv -path %credenlist%.csv
-ForEach-Object{get-adobject -filter "name -like '*$($_.name)*'" -properties *}
-Select-Object samaccountname|export-csv %output%.csv
-#convert to txt without first row (name) & put it into the var
-$var = Get-Content %somedir%\yourlist.txt
-#add users from txt to ADgroup
-Add-ADGroupMember "group" -Member $var
