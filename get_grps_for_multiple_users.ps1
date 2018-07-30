e4ce5fc1227bc6d93d3a7393dee59412719b8741
Maxim Chinenkof <flazh@yandex.ru>
2018-07-30 15:59:44 +0300

    Add files via upload

---
+++ get_grps_for_multiple_users.ps1
@@ -0,0 +1,18 @@
+$userlist = Get-Content 'C:\Users\mchinenkov\Desktop\logins.txt'
+
+
+Get-ADUser -Filter '*' -Properties memberof | Where-Object {
+ 
+	$userlist -contains $_.SamAccountName
+
+} | ForEach-Object {
+ 
+	$username = $_.SamAccountName
+ 
+	$groups = $_ | Select-Object -Expand memberof |
+
+		ForEach-Object { (Get-ADGroup $_).Name }
+ 
+	"{0}; {1}" -f $username, ($groups -join '; ')
+
+} | Out-File 'C:\Users\mchinenkov\Desktop\ss.csv'
