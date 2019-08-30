b0ff3fcab4e38a92e10026f1bec6562065cb93f5
Maxim Chinenkof <flazh@yandex.ru>
2019-08-30 15:07:24 +0300

    Update groups_expand.ps1

--- groups_expand.ps1
+++ groups_expand.ps1
@@ -20,9 +20,8 @@ function Get-ADPrincipalGroupMembershipRecursive( ) {
     return $groups
 }
 
-# Simple Example of how to use the function
 $username = Read-Host -Prompt "Enter a username"
-$groups   = Get-ADPrincipalGroupMembershipRecursive (Get-ADUser $username).DistinguishedName
+$groups = Get-ADPrincipalGroupMembershipRecursive (Get-ADUser $username).DistinguishedName
 #$groups | Sort-Object -Property name | Format-Table
 $groups | Select-Object name
 #| Measure-Object
