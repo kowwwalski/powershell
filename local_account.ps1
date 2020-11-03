71cac8b482f96553a0f5569d5fe6814b6b2ea85b
Maxim Chinenkof <flazh@yandex.ru>
2020-11-03 13:46:36 +0300

    Update local_account.ps1

--- local_account.ps1
+++ local_account.ps1
@@ -1,9 +1,11 @@
 # I am running this one via external .bat with powershell.exe -ExecutionPolicy Bypass -file "%~dp0\path\to\this-file.ps1"
-#add local account with adm privileges
-$user_name = Read-Host -Prompt "Type username"
-$user_fname = Read-Host -Prompt "Type full display name"
+# create local account in proper format and add it to adm group
 #
-#check username for limit (20 symbols)
+$name = Read-Host -Prompt "Input name"
+$surname = Read-Host -Prompt "Input surname"
+$user_name = "$name.$surname"
+#
+# username section
 if (($user_name | Measure-Object -Character).Characters -gt 20) {
     $user_20sym = $user_name.Substring(0, 20)
     Write-Host "Account will be created with name $user_20sym"
@@ -13,6 +15,22 @@ else {
     Write-Host "Account will be created with name $user_20sym"
 }
 #
+# check if user already exists
+$user_check = Get-LocalUser -Name $user_20sym -ErrorAction SilentlyContinue
+    if (-not $user_check) {
+        Write-Host "OK. Account can be created"
+    }
+    else {
+        Write-Host "User exist! Check account manually or choose another name."
+        Exit
+    }
+#
+# full display name section
+$fullname = $name.Substring(0,1).ToUpper()+$name.Substring(1).ToLower()
+$fullsurname = $surname.Substring(0,1).ToUpper()+$surname.Substring(1).ToLower()
+$full_display_name = "$fullsurname, $fullname"
+Write-Host "The display name is $full_display_name"
+#
 #pwd section, yepp
 $user_password = Read-Host -Prompt "Type password" -AsSecureString
 $pass_confirm = Read-Host -Prompt "Confirm password" -AsSecureString
@@ -20,17 +38,8 @@ $user_password_txt = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime
 $pass_confirm_txt = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($pass_confirm))
 #
 if ($user_password_txt -ceq $pass_confirm_txt) {
-    $user_check = Get-LocalUser -Name $user_20sym -ErrorAction SilentlyContinue
-    if (-not $user_check) {
-        Write-Host "User not found, configuring..."
-        New-LocalUser -Name $user_20sym -FullName $user_fname -Password $user_password
-    }
-    else {
-        Write-Host "User exist! Check account manually or choose another name."
-    }
+    New-LocalUser -Name $user_20sym -FullName $full_display_name -Password $user_password
     sleep 2
-    #
-    #add to local groups
     Add-LocalGroupMember -Group "Users" -Member $user_20sym
     Add-LocalGroupMember -Group "Administrators" -Member $user_20sym
 }
